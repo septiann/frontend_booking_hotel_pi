aaa013bd36470e794e12ccd526dc17eeb174ba96
Septian Nugraha <septiann@outlook.com>
2021-03-31 18:54:41 +0700

    component stepper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import LandingPage from 'pages/LandingPage'
 import DetailsPage from 'pages/DetailsPage'
+import Checkout from 'pages/Checkout'
 import Example from 'pages/Example'
 
 import 'assets/scss/style.scss'
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Route exact path="/" component={LandingPage}></Route>
         <Route exact path="/properties/:id" component={DetailsPage}></Route>
+        <Route exact path="/checkout" component={Checkout}></Route>
         <Route path="/example" component={Example}></Route>
       </Router>
   </div>
---
+++ src/elements/Stepper/Controller/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Controller(props) {
+    return (
+        <Fade>
+            <section className="container">
+                <div className="row justify-content-center">
+                    <div className="col-3">{props.children}</div>
+                </div>
+            </section>
+        </Fade>
+    )
+}
